f1535eddf6f5775e90c1d547be39a439752d7540
Sanika Rajpure <sanika.rajpure@gmail.com>
2024-12-04 16:28:07 +0530

    cors,routes and DB sync set up

--- server.js
+++ server.js
@@ -3,32 +3,44 @@ const app = express();
 const cors = require("cors");
 const xss = require("xss-clean");
 require("dotenv").config();
+const routes = require("./routes");
+const { handleError, convertToApiError } = require("./utils/apiError");
+const sequelize = require("./db");
 
 // CORS
-if (process.env.NODE_ENV === "production") {
-  app.use(
-    cors({
-      origin: process.env.ORIGIN_FOR_CORS,
-      methods: ["POST", "GET", "PUT", "DELETE"],
-      credentials: true,
-    })
-  );
-} else {
-  app.use(
-    cors({
-      origin: "*",
-      methods: ["POST", "GET", "PUT", "DELETE", "PATCH"],
-      allowedHeaders: ["Authorization", "Content-Type"],
-      credentials: true,
-    })
-  );
-}
+app.use(
+  cors({
+    origin: process.env.ORIGIN_FOR_CORS,
+    methods: ["POST", "GET", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Authorization", "Content-Type"],
+    credentials: true,
+  })
+);
 
 //BODY PARSER
 app.use(express.json());
 
 //SANITIZE JSON
 app.use(xss());
+
+//SYNC DATABASE
+sequelize
+  .sync()
+  .then(() => {
+    console.log("Database synced successfully.");
+  })
+  .catch((err) => {
+    console.error("Error syncing database:", err);
+  });
+
+//ROUTES
+app.use("/api", routes);
+
+//API ERROR HANDLING
+app.use(convertToApiError);
+app.use((err, req, res, next) => {
+  handleError(err, res);
+});
 
 const port = process.env.PORT || 3002;
 app.listen(port, () => {
