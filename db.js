0dc697939fe2f808a4657f122747c7a34651bf65
Sanika Rajpure <sanika.rajpure@gmail.com>
2024-12-04 16:27:40 +0530

    sequelize set up

---
+++ db.js
@@ -0,0 +1,23 @@
+const { Sequelize } = require("sequelize");
+require("dotenv").config(); 
+
+// Initialize Sequelize with environment variables
+const sequelize = new Sequelize({
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  dialect: "mysql",
+});
+
+// Test the connection
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Database connection established successfully.");
+  })
+  .catch((error) => {
+    console.error("Unable to connect to the database:", error);
+  });
+
+module.exports = sequelize;
