61cbf66a951a9577bd775087ae4eeee0cfef8031
Sanika Rajpure <sanika.rajpure@gmail.com>
2024-12-05 13:36:53 +0530

    added winstom for logging

---
+++ utils/logger.js
@@ -0,0 +1,23 @@
+// logger.js
+const winston = require("winston");
+
+const logger = winston.createLogger({
+  level: "info",
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+      ),
+    }),
+    new winston.transports.File({
+      filename: "logs/app.log",
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+      ),
+    }),
+  ],
+});
+
+module.exports = logger;
